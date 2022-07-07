2d58603d00351ed80f811db945995fa671d2c16d
amsnir062 <106139936+amsnir062@users.noreply.github.com>
2022-07-07 15:16:56 +0545

    Update Untitled-1.c

--- Untitled-1.c
+++ Untitled-1.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <conio.h>
+void main()
 {
     int i,num[10];
     clrscr();
     printf("Enter the 10 number:\t");
-    for(i=0;i<10;1++)
+    for(i=0;i<10;i++)
     {
         scanf("%d",&num[i]);
     }
     printf("\nThe 10 numbers are:\t");
-     for(i=0;i<10;1++)
+    for(i=0;i<10;i++)
     {
         printf("%d\t",&num[i]);
     }
