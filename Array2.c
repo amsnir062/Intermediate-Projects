9ee162ab7f48a7e1fd69d2a8b9889628e79e9c84
amsnir062 <106139936+amsnir062@users.noreply.github.com>
2022-07-07 15:17:29 +0545

    Add files via upload
    college project

---
+++ Array2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+{
+    int i,sum=0;
+    int num[5]={4,5,6,7,8};
+    printf("\nThe sum of five numbers is:\t");
+     for(i=0;i<5;i++)
+    {
+        printf("%d\t",&num[i]);
+        sum=sum+num[i];
+    }
+    printf("\n The sum of 5 numbers is:%d",sum);
+    getch();
+}
